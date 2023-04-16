ac63f15df6ce1d3bb4d1a59e55b1fac5152c2e07
Danny Matlob <44306495+DannyMatlob@users.noreply.github.com>
2023-04-15 20:52:50 -0700

    added dictionary definition support

--- SpellingBee/spell.py
+++ SpellingBee/spell.py
@@ -4,12 +4,13 @@ import os
 import pygame
 import keyboard
 import random
+import requests
 
 print(os.getcwd())  # print the current directory
 print(os.listdir())  # print a list of files in the current directory
 # Define the list of words to spell out
 words = []
-with open('dictionary.txt', 'r') as file:
+with open('dictionary7-9.txt', 'r') as file:
     for line in file:
         word = line.strip()  # remove the newline character
         words.append(word)  # add the word to the list
@@ -30,6 +31,19 @@ def playSound():
             break
         pass
 
+def getDefinition(word):
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # The request was successful
+        data = response.json()
+        print(data[0]["meanings"][0]["definitions"][0]["definition"])
+        return data[0]["meanings"][0]["definitions"][0]["definition"]
+    else:   
+        # The request was not successful
+        print("Error: {}".format(response.status_code))
+
 # Loop through the words in the list and spell them out letter by letter
 for word in words:
     tts = gTTS(word, lang='en')
@@ -37,17 +51,14 @@ for word in words:
     print(word)
     playSound()
     #print("Press enter to spell word")
-    input() # Wait for user input before next word
-    
-    #print("Press enter for next word")
-    """
-    for letter in word:
-        tts = gTTS(letter, lang='en')
+    recvInput = input() # Wait for user input before next word
+    if (recvInput == "d"):
+        definition = getDefinition(word)
+        tts = gTTS(definition, lang='en')
         tts.save('sound.mp3')
         playSound()
-        if keyboard.is_pressed('enter'):
-            break
-    input() # Wait for user input before next word
-    """
+        input()
+    
+    #print("Press enter for next word")
     print("-------NEXT WORD---------")
 
